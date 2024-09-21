033d20cbd56746da39ff48032f9b2bc30d7b9531
Sevada Shaghoolian <sh.sevada@gmail.com>
2024-09-20 19:54:37 -0700

    changed the book title in test_client.py

--- test_client.py
+++ test_client.py
@@ -1,7 +1,7 @@
 from book_fetcher import search_book
 
 # Example book title to search and insert into the database
-book_title = "J.R.R. Tolkien"
+book_title = "who moved my cheese"
 
 # Call the search_book function to fetch and insert the book
 search_book(book_title)
